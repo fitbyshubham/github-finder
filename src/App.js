61c3b679e0a14861c146e8ae4300fc36ff17c904
Shubham Sharma <shubhamshrm02@gmail.com>
2021-05-07 20:38:08 +0530

    Used CBC, before refactoring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import './App.css'
+import Users from "./components/users/Users";
+import User from "./components/users/User";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import axios from "axios";
+import Search from "./components/users/Search";
+import Alert from "./components/layouts/Alert";
+import About from "./components/pages/About";
+
+
+class App extends Component{
+    state = {
+        users: [],
+        user: {},
+        repos: [],
+        loading: false,
+        alert: null
 }
 
-export default App;
+
+//Search Github Users
+
+    searchUsers = async text => {
+        this.setState({loading: true})
+        const res= await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${
+            process.env.REACT_APP_CLIENT_ID
+        }&client_secret=${process.env.REACT_APP_CLIENT_SECRET_ID}`)
+        this.setState({users: res.data.items, loading: false})
+
+    }
+
+    //Get Users Repos
+    getUserRepos = async (username) => {
+        this.setState({loading: true})
+        const res= await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+            process.env.REACT_APP_CLIENT_ID
+        }&client_secret=${process.env.REACT_APP_CLIENT_SECRET_ID}`)
+        this.setState({repos: res.data, loading: false})
+    }
+
+    //Get Single Github User
+
+    getUser = async (username) => {
+        this.setState({loading: true})
+        const res= await axios.get(`https://api.github.com/users/${username}?client_id=${
+            process.env.REACT_APP_CLIENT_ID
+        }&client_secret=${process.env.REACT_APP_CLIENT_SECRET_ID}`)
+        this.setState({user: res.data, loading: false})
+    }
+
+
+
+    //clear search dialog
+
+    clearUsers = () => {
+        this.setState({users: [], loading: false})
+    }
+
+    // Set Alert
+    setAlert = (msg, type) => {
+        this.setState({alert: {msg, type}})
+
+        setTimeout(() => {this.setState({alert: null})}, 5000)
+
+    }
+    cancelButton = () =>{
+        this.setState({alert: null})
+    }
+
+
+    render(){
+        return(
+            <Router>
+                <Navbar/>
+
+                <div className='container'>
+                    <Alert alert={this.state.alert} />
+                    <Switch>
+                        <Route exact path="/" render={props => (
+                            <>
+                                <Search searchUsers={this.searchUsers}
+                                        clearUsers={this.clearUsers}
+                                        showClear={this.state.users.length > 0 ? true: false}
+                                        setAlert={this.setAlert}
+                                />
+
+                                <Users loading={this.state.loading} users={this.state.users}/>
+                            </>
+                        )}/>
+                        <Route exact path ="/about" component={About}/>
+                        <Route exact path="/user/:login" render={ props => (
+                            <User {...props}
+                                  getUser={this.getUser}
+                                  user={this.state.user}
+                                  loading={this.state.loading}
+                                  getUserRepos={this.getUserRepos}
+                                  repos={this.state.repos}
+                            />
+
+                        )
+
+                        }/>
+                    </Switch>
+
+
+                </div>
+
+
+            </Router>
+
+
+        )
+    }
+}
+
+
+
+export default App
---
+++ src/components/users/User.js
@@ -0,0 +1,111 @@
+import React, {Component, Fragment} from 'react'
+import Spinner from "../layouts/Spinner";
+import PropTypes from "prop-types";
+import {Link} from 'react-router-dom'
+import Repos from '../repos/Repos'
+
+class User extends Component{
+
+    componentDidMount() {
+        this.props.getUser(this.props.match.params.login)
+        this.props.getUserRepos(this.props.match.params.login)
+    }
+
+    static propTypes ={
+        loading: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        repos: PropTypes.array.isRequired,
+        getUser: PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired
+    }
+
+    render(){
+        const {
+            name,
+            avatar_url,
+            location,
+            bio,
+            blog,
+            login,
+            html_url,
+            followers,
+            following,
+            public_repos,
+            public_gists,
+            hireable,
+            company
+        } = this.props.user
+
+        const {loading,repos} = this.props
+
+
+        //this if stat is not working
+        if(loading) return <Spinner />
+
+        return(
+            <>
+            < Link to='/' className="btn btn-light">
+                Back to Search
+            </Link>
+            Available:{hireable ? 'true' : 'false'
+                }
+                <div className="card grid-2">
+                    <div className="all-center">
+                        <img src={avatar_url} alt="" className="round-img" style={{width: '150px'}}/>
+                        <h1> {name}</h1>
+                        <p>Location: {location}</p>
+                    </div>
+                    <div >{bio && <>
+                    <h3>Bio</h3>
+                        <p>{bio}</p>
+                    </>}
+                    <a href={html_url} className="btn btn-dark my-1">Visit github profile</a>
+                        <ul>
+                            <li>
+                                {login && <Fragment>
+                                <strong>Username: </strong>{login}
+                                </Fragment>}
+                            </li>
+                            <li>
+                                {company && <Fragment>
+                                    <strong>Company: </strong>{company}
+                                </Fragment>}
+                            </li>
+                            <li>
+                                {blog && <Fragment>
+                                    <strong>Website: </strong>{blog}
+                                </Fragment>}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="card text-center">
+                    <div className="badge badge-primary">
+                        Followers: {followers}
+                    </div>
+                    <div className="badge badge-success">
+                        Following: {following}
+                    </div>
+                    <div className="badge badge-light">
+                        Public repos: {public_repos}
+                    </div>
+                    <div className="badge badge-dark">
+                        Public Gists: {public_gists}
+                    </div>
+
+
+                </div>
+                <Repos repos={repos} />
+
+                </>
+
+        )
+
+
+
+    }
+}
+
+export default User
+
